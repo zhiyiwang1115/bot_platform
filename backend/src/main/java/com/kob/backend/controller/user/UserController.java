f56164d3cefbc5f22e6b9e72482e5f1591a6ffd3
zhiyiwang <zhiyiwang1115@gmail.com>
2023-05-02 16:49:41 +1000

    some database and security config at backend

---
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -0,0 +1,60 @@
+package com.kob.backend.controller.user;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Wrapper;
+import java.util.List;
+
+//4 layers in springboot
+//pojo: database table => class
+//mapper: curd in class => sql (basic operations)
+//service: implement service (more complicated, may involve multiple mappers)
+//controller: schedule service according to the data from frontend
+
+//Controller + Response Body
+@RestController
+public class UserController {
+
+    // when use mapper, must put autowired here
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @GetMapping("/user/all/")
+    public List<User> getAll(){
+        return userMapper.selectList(null);
+    }
+
+    //case when parameter in the url
+    @GetMapping("/user/{userId}/")
+    public User getUser(@PathVariable int userId){
+        //grammar for select with condition
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id",userId);
+        return userMapper.selectOne(queryWrapper);
+    }
+
+    @GetMapping("/user/add/{userId}/{username}/{password}/")
+    public String addUser(@PathVariable int userId,
+                          @PathVariable String username,
+                          @PathVariable String password){
+        User user = new User(userId,username,passwordEncoder.encode(password),null);
+        userMapper.insert(user);
+        return "success";
+    }
+
+    @GetMapping("/user/delete/{userId}")
+    public String deleteUser(@PathVariable int userId){
+        userMapper.deleteById(userId);
+        return "success";
+    }
+}
--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,19 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("pc"));
 	}
 
 }
