0055553952c8ab3716f431f9d493634d3f7ed1b4
zhiyiwang <zhiyiwang1115@gmail.com>
2023-05-04 02:42:41 +1000

    backend bot crud

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -4,11 +4,93 @@
 
 <script>
 import ContentField from '@/components/ContentField.vue'
+import $ from 'jquery'
+//need to import global store if we need token
+import { useStore } from 'vuex';
+
 
 export default{
     components: {
         ContentField,
+    },
+    setup(){
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "post",
+        //     data: {
+        //         title: "test8",
+        //         description: "test2",
+        //         content: "test3",
+        //     },
+        //     //pay attention, this should be headers rather than header
+        //     headers:{
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     }
+        // });
+
+        $.ajax({
+            url: "http://127.0.0.1:3000/user/bot/remove/",
+            type: "post",
+            data: {
+                bot_id: 1,
+            },
+            //pay attention, this should be headers rather than header
+            headers:{
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp){
+                console.log(resp);
+            },
+            error(resp){
+                console.log(resp);
+            }
+        });
+
+        //         $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/update/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 6,
+        //         title: "test_revised",
+        //         description: "test_revised_2",
+        //         content: "test_revised_3",
+        //     },
+        //     //pay attention, this should be headers rather than header
+        //     headers:{
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     }
+        // });
+
+        $.ajax({
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
+            //pay attention, this should be headers rather than header
+            headers:{
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp){
+                console.log(resp);
+            },
+            error(resp){
+                console.log(resp);
+            }
+        });
     }
+
+    
 }
 </script>
 
