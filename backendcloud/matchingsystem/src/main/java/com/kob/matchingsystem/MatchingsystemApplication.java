95213ad599001976f88758d34c6dd46802619bae
zhiyiwang <zhiyiwang1115@gmail.com>
2023-05-08 03:02:57 +1000

    start implementing matching system microservice

---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/MatchingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.kob.matchingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingsystemApplication.class, args);
+    }
+
+}
